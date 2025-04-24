30b6172f188cbe64b55aa4f9a65c22db7babf14e
sai3639 <saira3639@gmail.com>
2025-04-24 06:47:29 -0500

    d

--- api/migrations/20250422085416_add_binary_data_column_to_telemetry.js
+++ api/migrations/20250422085416_add_binary_data_column_to_telemetry.js
@@ -1,9 +1,12 @@
 exports.up = function(knex) {
     return knex.schema.alterTable('telemetry', table => {
-      table.binary('binary_data'); //binary data to display on frontend stored here - mysql
+      if (!exists) {
+        return knex.schema.table('telemetry', function(table) {
+          table.binary('binary_data'); //binary data to display on frontend stored here - mysql
+        });
+      }
     });
   };
-  
   exports.down = function(knex) {
     return knex.schema.alterTable('telemetry', table => {
       table.dropColumn('binary_data');
